98853018880b25e99f84106c1860164c2cf8d649
James Riall <jriall@google.com>
2018-03-03 23:35:59 +0000

    Completed lesson 26

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const authController = require('../controllers/authController');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post('/add',
     storeController.upload,
     catchErrors(storeController.resize),
@@ -26,6 +26,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login)
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -36,5 +37,10 @@ router.post('/register',
     userController.register,
     authController.login
 )
+
+router.get('/logout', authController.logout);
+
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
 
 module.exports = router;
