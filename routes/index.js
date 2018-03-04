ad6f9d91a2e89a26ade57096a930e95c3e9d772b
James Riall <jriall@google.com>
2018-03-04 15:25:24 +0000

    Completed lesson 36

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const router = express.Router();
 const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
+const router = express.Router();
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', authController.isLoggedIn, storeController.addStore);
-router.post('/add',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.createStore)
+router.post(
+  '/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore),
 );
-router.post('/add/:id',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.updateStore));
+router.post(
+  '/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore),
+);
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
@@ -32,11 +35,12 @@ router.get('/register', userController.registerForm);
 // 1. Validate the registration data
 // 2. Register the user
 // 3. Log them in
-router.post('/register',
-    userController.validateRegister,
-    userController.register,
-    authController.login
-)
+router.post(
+  '/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login,
+);
 
 router.get('/logout', authController.logout);
 
@@ -44,14 +48,16 @@ router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount));
 router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
-router.post('/account/reset/:token',
-    authController.confirmedPasswords,
-    catchErrors(authController.update)
+router.post(
+  '/account/reset/:token',
+  authController.confirmedPasswords,
+  catchErrors(authController.update),
 );
-
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 module.exports = router;
